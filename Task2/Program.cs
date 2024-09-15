3e3942ac06b7cd00ca8d19e8369abda1910aa088
isalexeeva <alexeeva.is4@yandex.ru>
2024-09-15 13:21:06 +0700

    Add sol from HW7

---
+++ Task2/Program.cs
@@ -0,0 +1,45 @@
+﻿// // See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// Задача 2: Функция Аккермана
+// Описание: Напишите программу для вычисления функции Аккермана с помощью
+// рекурсии. Даны два неотрицательных числа m и n.
+// Пример:
+// ● Вход: m = 2, n = 3
+// ● Выход: A(m, n) = 29
+// ● Вход: m = 1, n = 4
+// ● Выход: A(m, n) = 7
+
+// Подсказка: Реализуйте рекурсивную функцию Аккермана, основываясь на её
+// определении:
+// ● A(0,n)=n+1A(0, n) = n + 1A(0,n)=n+1
+// ● A(m,0)=A(m−1,1)A(m, 0) = A(m - 1, 1)A(m,0)=A(m−1,1)
+// ● A(m,n)=A(m−1,A(m,n−1))A(m, n) = A(m - 1, A(m, n - 1))A(m,n)=A(m−1,A(m,n−1))
+
+
+
+void Main()
+{
+    int m = ReadInt("Введите число m: ");
+    int n = ReadInt("Введите число n: ");
+    
+    System.Console.WriteLine($"A({m}, {n}) = {Ackermann(m, n)}");
+}
+
+int Ackermann(int m, int n)
+{
+    if (m == 0)
+        return n + 1;
+    else if (m > 0 && n == 0)
+        return Ackermann(m - 1, 1);
+    else if (m > 0 && n > 0)
+        return Ackermann(m - 1, Ackermann(m, n - 1));
+    return 0; 
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
---
+++ Task3/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задача 3: Вывод элементов массива в обратном порядке
+// Описание: Задайте произвольный массив. Выведите его элементы, начиная с конца.
+// Используйте рекурсию и не используйте циклы.
+
+// Подсказка: Используйте рекурсию, чтобы выводить элементы массива начиная с
+// последнего и до первого.
+
+void Main()
+{
+    int sizeOfArray = ReadInt("Введите размер массива: ");
+    int[] array = GenetateArray(sizeOfArray, 0, 10);
+    PrintArray(array);
+    PrintArrayReverse(array, sizeOfArray-1);
+
+}
+
+void PrintArrayReverse(int[] array, int i)
+{
+    if (i < 0) 
+        return; 
+    Console.Write(array[i]);
+    if (i > 0) Console.Write(" ");
+    PrintArrayReverse(array, i - 1);
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.Length; i++)
+        System.Console.Write(arrayForPrint[i] + " ");
+    System.Console.WriteLine();
+}
+
+int[] GenetateArray(int size, int minRange, int maxRange)
+{
+    Random rnd = new Random();
+    int[] tempArray = new int[size];
+    for (int i = 0; i < tempArray.Length; i++)
+        tempArray[i] = rnd.Next(minRange, maxRange +1);
+    return tempArray;
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
